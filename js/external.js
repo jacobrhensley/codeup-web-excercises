09c5dd31f4fe3e022524b2aff22e3d57ba5a701e
Jacob Hensley <jacobrhensley3@gmail.com>
2020-03-16 16:58:36 -0500

    committing additional JS exercise for today

---
+++ js/external.js
@@ -0,0 +1,7 @@
+console.log("Hello, from external JavaScript!");
+
+alert("Welcome to my Website!");
+
+var response = prompt("What is your favorite color?")
+
+alert("Great, " + response + " is my favorite color too!")
