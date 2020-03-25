cd656e81db37dced1d0f29a747e4d56594e2fb09
Jacob Hensley <jacobrhensley3@gmail.com>
2020-03-25 10:35:48 -0500

    completed for-loops exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,5 +1,27 @@
-function showMultiplicationTable(number){
-    for (i = 1; i<= 10; i++){
-
+function showMultiplicationTable(number) {
+    for (i = 1; i <= 10; i++) {
+        console.log(number + ' x ' + i + ' = ' + (number * i));
     }
+}
+
+showMultiplicationTable(9);
+showMultiplicationTable(12);
+
+
+    for (i = 1; i <= 10; i++) {
+        var random = Math.floor(Math.random() * 200) + 1;
+        if(random % 2 === 0){
+        console.log(random + ' is even');
+            } else
+                console.log(random + ' is odd')
+    }
+
+    for (i = 1; i < 10; i++){
+        console.log(i.toString().repeat(i));
+    }
+
+
+for (i = 100; i >= 5; i - 5){
+    console.log(i);
+    i -= 5;
 }
