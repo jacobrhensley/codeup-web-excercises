75701186032f53734bbd322748a5c7db71f4caf6
Jacob Hensley <jacobrhensley3@gmail.com>
2020-03-25 15:00:33 -0500

    completed exercise on iterating-arrays

--- js/for_loops.js
+++ js/for_loops.js
@@ -8,20 +8,20 @@ showMultiplicationTable(9);
 showMultiplicationTable(12);
 
 
-    for (i = 1; i <= 10; i++) {
-        var random = Math.floor(Math.random() * 200) + 1;
-        if(random % 2 === 0){
+for (i = 1; i <= 10; i++) {
+    var random = Math.floor(Math.random() * 200) + 1;
+    if (random % 2 === 0) {
         console.log(random + ' is even');
-            } else
-                console.log(random + ' is odd')
-    }
+    } else
+        console.log(random + ' is odd')
+}
 
-    for (i = 1; i < 10; i++){
-        console.log(i.toString().repeat(i));
-    }
+for (i = 1; i < 10; i++) {
+    console.log(i.toString().repeat(i));
+}
 
 
-for (i = 100; i >= 5; i - 5){
+for (i = 100; i >= 5; i - 5) {
     console.log(i);
     i -= 5;
 }
