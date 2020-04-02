4fa9e757118d2a636d3b0e966e173c0011bc67e0
Jacob Hensley <jacobrhensley3@gmail.com>
2020-04-02 09:57:07 -0500

    made working map

---
+++ js/mapbox-api-lec.js
@@ -0,0 +1,16 @@
+/**********************************************
+ * 			SETTING UP KEYS.JS
+ *********************************************/
+// TODO TOGETHER: Open .gitignore and add keys.js. Add keys.js file and import to mapbox html file. Your api keys are stored in keys.js and are added to the .gitignore so they are protected
+
+/**********************************************
+ * 			CUSTOMIZING THE MAP
+ *********************************************/
+// Predefined map styles --> https://docs.mapbox.com/mapbox-gl-js/api/#map
+
+// A map center can be set by passing in the latitude and longitude coordinates of a location as an array [LONGITUDE_VALUE, LATITUDE_VALUE]
+
+// Zoom levels range from 0 up to 24, with 0 being a global view and 24 being the most detailed at street level (the max zoom level depends on the location).
+
+//TODO TOGETHER: Set map to Dallas area using the coordinates [-96.8057, 32.7787]
+
